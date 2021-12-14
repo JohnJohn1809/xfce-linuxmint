73f4edbdf4f3af9cc15934415734f4b336243e91
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-12-14 14:51:30 -0300

    Update 06-appearance.sh

--- scripts/06-appearance.sh
+++ scripts/06-appearance.sh
@@ -7,6 +7,10 @@ sudo apt install papirus-icon-theme -y
 sudo apt install papirus-folders -y
 sudo apt install libreoffice-style-papirus -y
 
+# Libreoffice Theme
+
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/install-papirus-root.sh | sh
+
 # Local Gtk Theme
 
 cd -
