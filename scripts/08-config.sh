dba2c48c28b87f849b99598546798bce89581b8c
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-12-14 14:42:20 -0300

    Update 08-config.sh

--- scripts/08-config.sh
+++ scripts/08-config.sh
@@ -5,3 +5,7 @@ cp -R /$HOME/posinstall-linuxmint/config/.config/* "/$HOME/.config/"
 cp -R /$HOME/posinstall-linuxmint/config/home/* "/$HOME/"
 
 sudo cp -R /$HOME/posinstall-linuxmint/config/etc/* "/etc/"
+
+cd /$HOME/scripts/
+
+chmod +x clean.sh upgrade.sh
