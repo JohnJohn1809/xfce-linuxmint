fc41177f1ddc27b7564921fb89f39a2acc03f44f
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-12-16 20:22:20 -0300

    Update 06-appearance.sh

--- scripts/06-appearance.sh
+++ scripts/06-appearance.sh
@@ -13,10 +13,43 @@ wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libre
 
 wget -qO- https://git.io/papirus-folders-install | sh
 
-# Local Gtk Theme
+# Local Gtk Themes
 
-cd -
-cd posinstall-linuxmint/gtk-themes/icons/
+# Download
+
+cd $HOME
+
+wget -c 'https://www.dropbox.com/sh/dlp00bm655omkyf/AADexsVnIBNA-daaLw-zkmb2a?dl=0'
+
+# Extracting
+
+mv AADexsVnIBNA-daaLw-zkmb2a?dl=0 gtk-themes.zip
+
+mkdir gtk-themes
+
+unzip gtk-themes.zip -d $HOME/gtk-themes/
+
+rm gtk-themes.zip
+
+# Moving
+
+cd $HOME/gtk-themes/icons/
+
 sudo tar xvzf capitaine-cursor-dark-r4.tar.gz -C /usr/share/icons/
-cd -
-cd posinstall-linuxmint/scripts/
+
+cd $HOME
+
+rm -rf gtk-themes
+
+cd $HOME/posinstall-linuxmint/scripts/
+
+------------------------------[old]----------------------------------
+
+#cd -
+#cd posinstall-linuxmint/gtk-themes/icons/
+#sudo tar xvzf capitaine-cursor-dark-r4.tar.gz -C /usr/share/icons/
+#cd -
+#cd posinstall-linuxmint/scripts/
+
+------------------------------[old]----------------------------------
+
