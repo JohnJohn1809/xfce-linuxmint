f50d79f56ebbc3011f2b44d6c16f933f33f554d9
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-12-17 10:32:39 -0300

    Update 09-config.sh

--- scripts/09-config.sh
+++ scripts/09-config.sh
@@ -7,7 +7,7 @@ mkdir $HOME/Downloads/ClipGrab
 mkdir $HOME/Downloads/4kstogram
 mkdir $HOME/Downloads/4kvideodownloader
 mkdir $HOME/Downloads/Warpinator
-mkdir $HOME/Downloads/Telegram Desktop
+mkdir $HOME/Downloads/Telegram
 mkdir $HOME/Pictures/Screenshots
 
 # All
