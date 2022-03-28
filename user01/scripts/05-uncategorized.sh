1fa16fa0bd24545293682f6ecc4030b1f3d2fbae
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2022-03-28 08:16:01 -0300

    Update 05-uncategorized.sh

--- user01/scripts/05-uncategorized.sh
+++ user01/scripts/05-uncategorized.sh
@@ -3,7 +3,7 @@
 sudo apt install plank -y
 sudo apt install numlockx -y
 sudo apt install neofetch -y
-#sudo apt install keepassxc -y
+sudo apt install keepassxc -y
 sudo apt install gparted -y
 sudo apt install grub-customizer -y
 sudo apt install simplescreenrecorder -y
