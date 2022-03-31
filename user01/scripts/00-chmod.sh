122faa4ad30d42130959c6ed028399b35e81ad67
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2022-03-31 11:26:00 -0300

    Update 00-chmod.sh

--- user01/scripts/00-chmod.sh
+++ user01/scripts/00-chmod.sh
@@ -2,4 +2,4 @@
 
 # Set Scripts Executables
 
-chmod +x 01-depedences.sh 02-midia.sh 03-documents.sh 04-web-browser.sh 05-uncategorized.sh 06-unrepository-apps.sh 07-flatpak.sh 08-appearance.sh 09-config.sh
+chmod +x 01-depedences.sh 02-midia.sh 03-documents.sh 04-web-browser.sh 05-uncategorized.sh 06-unrepository-apps.sh 07-flatpak.sh 08-appearance.sh 09-config.sh 10-create-new-users.sh
