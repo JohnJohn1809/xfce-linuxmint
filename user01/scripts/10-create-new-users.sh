0872371b8f611efb4b526e3b456bb60938f50905
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2022-04-01 11:58:01 -0300

    Update 10-create-new-users.sh

--- user01/scripts/10-create-new-users.sh
+++ user01/scripts/10-create-new-users.sh
@@ -7,3 +7,32 @@ sudo useradd -m users patricia
 
 sudo passwd junior
 sudo passwd patricia
+
+# Make Folders
+
+sudo mkdir /home/junior/.config/
+sudo mkdir /home/junior/.local/
+sudo mkdir /home/junior/.local/share/
+sudo mkdir /home/junior/.local/share/applications/
+sudo mkdir /home/patricia/.config/
+sudo mkdir /home/patricia/.local/
+sudo mkdir /home/patricia/.local/share/
+sudo mkdir /home/patricia/.local/share/applications/
+
+# Moving Config
+
+# User 02
+
+sudo cp -R $HOME/xfce-linuxmint/config/user02/home/* "/home/junior/"
+
+sudo cp -R $HOME/xfce-linuxmint/config/user02/home/.config/* "/home/junior/.config/"
+
+sudo cp -R $HOME/xfce-linuxmint/config/user02/home/.local/share/applications/* "/home/junior/.local/share/applications/"
+
+# User03
+
+sudo cp -R $HOME/xfce-linuxmint/config/user03/home/* "/home/patricia/"
+
+sudo cp -R $HOME/xfce-linuxmint/config/user03/home/.config/* "/home/patricia/.config/"
+
+sudo cp -R $HOME/xfce-linuxmint/config/user03/home/.local/share/applications/* "/home/patricia/.local/share/applications/"
