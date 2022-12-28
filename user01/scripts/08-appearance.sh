4a699c1c2c052a939dae8f16628cd9b7929f8eb6
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2022-12-27 23:51:21 -0300

    Update 08-appearance.sh

--- user01/scripts/08-appearance.sh
+++ user01/scripts/08-appearance.sh
@@ -2,44 +2,44 @@
 
 # Papirus
 
-wget -qO- https://git.io/papirus-icon-theme-install | sh
+#wget -qO- https://git.io/papirus-icon-theme-install | sh
 
-wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/install-papirus-root.sh | sh
+#wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/install-papirus-root.sh | sh
 
 # Papirus Folders
 
-wget -qO- https://git.io/papirus-folders-install | sh
+#wget -qO- https://git.io/papirus-folders-install | sh
 
 # Local Gtk Themes
 
 # Download
 
-cd $HOME
+#cd $HOME
 
-wget -c 'https://www.dropbox.com/sh/dlp00bm655omkyf/AADexsVnIBNA-daaLw-zkmb2a?dl=0'
+#wget -c 'https://www.dropbox.com/sh/dlp00bm655omkyf/AADexsVnIBNA-daaLw-zkmb2a?dl=0'
 
 # Extracting
 
-mv AADexsVnIBNA-daaLw-zkmb2a?dl=0 gtk-themes.zip
+#mv AADexsVnIBNA-daaLw-zkmb2a?dl=0 gtk-themes.zip
 
-mkdir $HOME/gtk-themes/
+#mkdir $HOME/gtk-themes/
 
-unzip gtk-themes.zip -d $HOME/gtk-themes/
+#unzip gtk-themes.zip -d $HOME/gtk-themes/
 
-rm gtk-themes.zip
+#rm gtk-themes.zip
 
 # Moving
 
-cd $HOME/gtk-themes/icons/
+#cd $HOME/gtk-themes/icons/
 
-sudo tar xvzf capitaine-cursor-dark-r4.tar.gz -C /usr/share/icons/
+#sudo tar xvzf capitaine-cursor-dark-r4.tar.gz -C /usr/share/icons/
 
-cd $HOME
+#cd $HOME
 
-rm -rf gtk-themes
+#rm -rf gtk-themes
 
-cd $HOME/posinstall-linuxmint/user01/scripts/
+#cd $HOME/posinstall-linuxmint/user01/scripts/
 
 # Arc
 
-sudo apt install arc-theme -y
+#sudo apt install arc-theme -y
