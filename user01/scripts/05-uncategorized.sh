64039c341183262fcf5ecfd1910093d751dda1b5
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2025-06-25 23:17:04 -0300

    Update 05-uncategorized.sh

--- user01/scripts/05-uncategorized.sh
+++ user01/scripts/05-uncategorized.sh
@@ -9,6 +9,7 @@ sudo apt install easytag -y
 sudo apt install android-tools-adb android-tools-fastboot -y
 sudo apt install minder -y
 sudo apt install color-picker -y
+sudo apt install audacity -y
 #sudo apt install grub-customizer -y
 #sudo apt install simplescreenrecorder -y
 #sudo apt install simplescreenrecorder-lib:i386
