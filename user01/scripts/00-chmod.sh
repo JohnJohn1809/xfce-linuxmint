5084aec4c4b1a683c0b9e9588f5fba9c9def7543
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2025-06-27 11:08:45 -0300

    Delete user01 directory

--- user01/scripts/00-chmod.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Set Scripts Executables
-
-chmod +x 01-depedences.sh 02-midia.sh 03-documents.sh 04-web-browser.sh 05-uncategorized.sh 06-flatpak.sh 07-files-backup.sh 08-create-new-users.sh
--- user01/scripts/02-midia.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#sudo apt install vlc -y
-#sudo apt install spotify-client -y
-sudo apt install mpv -y
-sudo apt install kdenlive -y
-sudo apt install audacity -y
-
